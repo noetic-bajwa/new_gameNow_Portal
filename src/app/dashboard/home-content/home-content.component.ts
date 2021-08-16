9404fe4419a6e665475eba6e13cb0331165106d0
Bajwa <anus@noeticworld.com>
2021-08-16 10:12:28 +0500

    Removed Extra Game  Files

--- src/app/dashboard/home-content/home-content.component.ts
+++ src/app/dashboard/home-content/home-content.component.ts
@@ -2,22 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { AuthenticationGuard } from '../../authentication.guard';
 
-import { ACTIONGAMES } from '../../GAMES/actionGames';
-import { FEATUREDGAMES } from '../../GAMES/featuredGames';
-import { LEADERBOARDGAMES } from '../../GAMES/leaderBoardGames';
-import { ADVENTUREGAMES } from '../../GAMES/adventureGames';
-import { BOARDGAMES } from '../../GAMES/boardGames';
-import { CARDGAMES } from '../../GAMES/cardGames';
-import { EDUCATIONALGAMES } from '../../GAMES/educationalGames';
-import { FAMILYGAMES } from '../../GAMES/familyGames';
-import { SPORTSGAMES } from '../../GAMES/sportsGames';
-import { STRATEGYGAMES } from '../../GAMES/strategyGames';
-import { WORDGAMES } from '../../GAMES/wordGames';
-import { MEMORYGAMES } from '../../GAMES/memoryGames';
-import { MULTIPLAYERGAMES } from '../../GAMES/multiplayerGames';
-import { PUZZLEGAMES } from '../../GAMES/puzzleGames';
-import { POPULARGAMES } from '../../GAMES/popularGames';
-import { RECOMMENDEDGAMES } from '../../GAMES/recommendedGames';
+
 import { ALLGAMES } from 'src/app/GAMES/allGames';
 
 import SwiperCore, {Lazy,  Autoplay, EffectCoverflow,EffectCube,EffectFlip, Pagination } from "swiper/core";
@@ -33,23 +18,9 @@ declare let AOS: any;
 })
 export class HomeContentComponent implements OnInit {
 
-  FeaturedGames = FEATUREDGAMES;
-  LeaderBoardGames = LEADERBOARDGAMES;
   
-  ActionGames=ACTIONGAMES;
-  AdventureGames=ADVENTUREGAMES;
-  BoardGames=BOARDGAMES;
-  CardGames = CARDGAMES;
-  EducationalGames = EDUCATIONALGAMES;
-  FamilyGames = FAMILYGAMES;
-  SportsGames = SPORTSGAMES;
-  StrategyGames = STRATEGYGAMES;
-  WordGames = WORDGAMES;
-  MemoryGames = MEMORYGAMES;
-  MultiplayerGames = MULTIPLAYERGAMES;
-  PuzzleGames = PUZZLEGAMES;
-  PopularGames = PUZZLEGAMES;
-  RecommendedGames = RECOMMENDEDGAMES;
+  
+  
   AllGames = ALLGAMES;
   
   constructor( private gamesData:DataService,private authService:AuthenticationGuard) { 
