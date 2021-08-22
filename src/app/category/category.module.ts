f082015ce596249cc71913da9b55a70640fd3122
Bajwa <anus@noeticworld.com>
2021-08-23 03:09:55 +0500

    playGame

--- src/app/category/category.module.ts
+++ src/app/category/category.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { CategoryRoutingModule } from './category-routing.module';
 import { CategoryComponent } from './category.component';
 import { GamePlayComponent } from './game-play/game-play.component';
+import { LazyLoadImageModule } from 'ng-lazyload-image';
 
 console.warn("New category module Loaded")
 @NgModule({
@@ -13,7 +14,8 @@ console.warn("New category module Loaded")
   ],
   imports: [
     CommonModule,
-    CategoryRoutingModule
+    CategoryRoutingModule,
+    LazyLoadImageModule
   ]
 })
 export class CategoryModule { }
